4de4e573216fbaf62da94a0ef82e325007e97b33
xiaopanggege <175714259@qq.com>
2020-04-27 19:27:06 +0800

    mysql严格模式开启

--- xpgg_pro/settings.py
+++ xpgg_pro/settings.py
@@ -160,11 +160,11 @@ DATABASES = {
 
         'OPTIONS': {
         # 在mysql5.7以前需要加下面的来使得同步数据库时候不会出现严格模式的警告，5.7开始默认是严格模式了就可以省略
-        #     'init_command': "SET sql_mode= 'STRICT_TRANS_TABLES'",
+            'init_command': "SET sql_mode= 'STRICT_TRANS_TABLES'",
         # 如果InnoDB Strict Mode也是严格模式也需要加下面的
         #     'init_command': 'SET innodb_strict_mode=1',
         #     之前是通过utf8创建的数据库，所以下面的这个注释掉，如果用utf8mb4可以试着打开这个
-        #     'charset': 'utf8mb4',
+            'charset': 'utf8mb4',
         },
     }
 }
